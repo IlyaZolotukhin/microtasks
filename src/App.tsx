3eb40da5b8a2733b9257703f0fedd64f182033d0
IlyaZolotuhin <1zolotyhin1@gmail.com>
2023-05-30 19:24:34 +0700

    useState commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+//1 урок.вложенные компоненты
+import {Body} from './site/Body';
+import {Header} from "./site/Header";
+import {Footer} from "./site/Footer";
+/*//2 урок.метод map
+import {CarsType, NewComponent} from "./site/NewComponent";*/
 
+//1 урок.вложенные компоненты
+/*function App() {
+    return (
+        <>
+            <Header title = {'NEW HEADER'} />
+            <Body titleForBody = {'NEW Body'}/>
+            <Footer titleForFooter = {'NEW Footer'}/>
+        </>
+
+    );
+}*/
+
+/*//2 урок.метод map
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [topCars,setTopCars] = useState<CarsType[]>( [
+        {id: 1, manufacturer: 'BMW', model: 'm5cs'},
+        {id: 2, manufacturer: 'Mercedes', model: 'e63s'},
+        {id: 3, manufacturer: 'Audi', model: 'rs6'}
+    ]);
+    return (
+        <NewComponent topCars={topCars}/>
+
+    );
+}*/
+
+// useState
+function App() {
+
+    let[a, setA]=useState(1)
+
+    const onClickHandler=()=>{
+        setA(++a);
+    }
+
+    const onClickHandler2=()=>{
+        setA(0);
+    }
+
+    return (
+        <div className="App">
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler2}>delete</button>
+        </div>
+
+    );
 }
 
 export default App;
